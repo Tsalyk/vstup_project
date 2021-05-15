52810fb4d3a2240882ad946bdd3485ca79778003
Tim Kozak <tima.kozak@gmail.com>
2021-05-15 16:19:35 +0300

    changed html logic

--- chance_flask/main.py
+++ chance_flask/main.py
@@ -10,6 +10,8 @@ TEMPLATE_DIR = os.path.abspath('./templates/')
 STATIC_DIR = os.path.abspath('./statc/')
 
 
+SPECIALTY = ""
+
 # app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 app = Flask(__name__)
 
@@ -28,10 +30,56 @@ def chance_page():
     universities = ["Український Католицький Університет"]
     specialties = ABIT.get_university_specialties()
     user_specialty = ""
-    user_grades = [None, None, None, None]
+    user_grades = ['', '', '', '']
+    user_score = 0
+    user_university = universities[0]
+    user_percentage = "98 %"
 
     if request.method == 'POST':
         user_specialty = request.form.get('specialty')
+        exams = ABIT.get_exams_by_specialty(user_specialty)
+        info = ABIT.get_info_by_specialty(user_specialty)
+        return render_template("specialty.html", specialties=specialties, \
+        universities=universities, exams=exams, info=info, user_score=user_score, \
+        user_specialty=user_specialty, user_university=user_university, 
+        user_percentage=user_percentage)
+
+    exams = ABIT.get_exams_by_specialty(user_specialty)
+    info = ABIT.get_info_by_specialty(user_specialty)
+
+    # if request.method == 'POST':
+    #     exams = list(ABIT.get_exams_by_specialty(user_specialty).keys())
+    #     grade_1 = request.form.get(exams[0])
+    #     grade_2 = request.form.get(exams[1])
+    #     grade_3 = request.form.get(exams[2])
+    #     grade_4 = request.form.get(exams[3])
+
+    #     user_grades = [grade_1, grade_2, grade_3, grade_4]
+    #     print(user_grades)
+
+    # exams_dict = ABIT.get_exams_by_specialty(user_specialty)
+    # exams = list(exams_dict.keys())
+    # info = ABIT.get_info_by_specialty(user_specialty)
+
+    # if user_grades[0] and user_grades[1] and user_grades[2] and user_grades[3]:
+    #     user_score = ABIT.calculate_rating_grade(user_grades, exams_dict)
+    user_percentage = "98 %"
+
+
+    return render_template("chance.html", specialties=specialties, \
+        universities=universities, exams=exams, info=info, user_score=user_score, \
+        user_specialty=user_specialty, user_university=user_university, 
+        user_percentage=user_percentage)
+
+@app.route("/<specialty>")
+def chance_specialty(user_specialty):
+    """
+    Generates a webpage with count of books and information
+    table about all books for every given language.
+    """
+    universities = ["Український Католицький Університет"]
+    specialties = ABIT.get_university_specialties()
+    user_grades = ['', '', '', '']
 
     exams = ABIT.get_exams_by_specialty(user_specialty)
     info = ABIT.get_info_by_specialty(user_specialty)
@@ -58,12 +106,10 @@ def chance_page():
         user_score = ABIT.calculate_rating_grade(user_grades, exams_dict)
     user_percentage = "98 %"
 
-
-    return render_template("chance.html", specialties=specialties, \
+    return render_template("specialty.html", specialties=specialties, \
         universities=universities, exams=exams, info=info, user_score=user_score, \
         user_specialty=user_specialty, user_university=user_university, 
         user_percentage=user_percentage)
-
 
 def launch_website():
     """
